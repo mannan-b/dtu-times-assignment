eddee2133cf1e0fab3e91665dc762045454b0483
mannan-b <mannanbajaj@gmail.com>
2025-08-21 18:18:24 +0530

    Deployed backend

--- vite-react-tailwind-js/src/App.jsx
+++ vite-react-tailwind-js/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 
-const API_URL = 'http://localhost:4000'
+const API_URL = 'https://dtu-times-assignment.onrender.com'
 
 const emptyForm = {
   title: '',
